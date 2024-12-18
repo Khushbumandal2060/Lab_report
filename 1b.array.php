39c22f3149165fad493d5ed79832d7c0c91011da
Khushbumandal2060 <112657238+Khushbumandal2060@users.noreply.github.com>
2024-12-18 22:45:12 +0545

    Add files via upload
    "display array using print_r and var_dump"

---
+++ 1b.array.php
@@ -0,0 +1,12 @@
+<?php
+// Part B: Create an array
+$arrayVar = array("PHP", 7.4, true, 100);
+
+// Display array using print_r
+echo "<h3>Display array using print_r</h3>";
+print_r($arrayVar);
+
+// Display array using var_dump
+echo "<h3>Display array using var_dump</h3>";
+var_dump($arrayVar);
+?>
