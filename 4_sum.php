59f633bc803503b3f17a893da360f0bd0d8408df
Khushbumandal2060 <112657238+Khushbumandal2060@users.noreply.github.com>
2024-12-19 18:32:15 +0545

    Add files via upload

---
+++ 4_sum.php
@@ -0,0 +1,12 @@
+<?php
+function addNumbers($num1, $num2) {
+    // Calculate the sum of the two numbers
+    $sum = $num1 + $num2;
+    return $sum;
+}
+
+// Example usage
+$number1 = 10;
+$number2 = 20;
+echo "The sum of $number1 and $number2 is " . addNumbers($number1, $number2) . ".";
+?>
